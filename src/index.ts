08c8db03dfa18d5119ae588ec5b30d008b7a5354
aygyun94 <aygyun_94@outlook.de>
2022-04-01 00:08:52 +0200

    router and errorHandling added.

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,24 @@
  * Required External Modules
  */
 
-import * as dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
+
 /**
- * 
+ *
  * App Variables
  */
 
 dotenv.config();
 
-if(!process.env.PORT){
-    process.exit(1);
+if (!process.env.PORT) {
+  process.exit(1);
 }
-
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
@@ -28,6 +31,10 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 /**
  * Server Activation
